6136411e83b1b69750af1899f9b33080435686f0
ethanhal <ethanhal@buffalo.edu>
2021-09-27 17:51:12 -0400

    Small syntax change for getting the PORT

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import *
-import sys
 import os
 app = Flask(__name__)
 
@@ -11,5 +10,5 @@ def hello_world():
     return render_template("Login.html")
 
 if __name__ == '__main__':
-    port = int(sys.argv[1] if len(sys.argv) >1 else 5000
+    port = os.environ.get("PORT", 5000)
     app.run(host='0.0.0.0', port=port, debug=True)
