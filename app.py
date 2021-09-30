98fe146505b40a6f60122e42bf403415e02093c3
elstonla <elstonla@buffalo.edu>
2021-09-29 20:14:04 -0400

    Add demo code from lecture to connect postgresql

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ app = Flask(__name__)
 def hello_world():
     return render_template("Login.html")
 
+def database_test():
+    import psycopg2
+    db_config = os.environ['DATABASE_URL']
+    print(db_config)
+    conn = psycopg2.connect(db_config, sslmode='require')
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS test (food TEXT, rating INTEGER)")
+    cursor.execute("INSERT INTO test VALUES ('borgir', 9)")
+    cursor.execute("INSERT INTO test VALUES ('salad', 0)")
+    conn.commit()
+    rows = cursor.execute("SELECT * FROM test").fetchall()
+    print(rows)
+    conn.close()
 if __name__ == '__main__':
     port = os.environ.get("PORT", 5000)
     app.run(host='0.0.0.0', port=port, debug=True)
