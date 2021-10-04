25578dc9ddb35f25ff190150d63dc3bedac691a9
ethanhal <ethanhal@buffalo.edu>
2021-10-04 15:19:31 -0400

    Deleted debugging code and added simple logout button

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from flask import *
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from .models import User
 import sys
 
@@ -15,17 +15,6 @@ def initialize():
 
 @auth.route("/login", methods =['POST', 'GET'])
 def login():
-    
-    """
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        for user in fake_database:
-            if user['username'] == username and user['password'] == password:
-                authenticate(username,password)
-                return redirect('/')
-    """
 
     if request.method == 'POST':
         username = request.form['username']
@@ -35,8 +24,6 @@ def login():
 
         
         if not user or not check_password_hash(user.password, password):
-            sys.stderr.write(str(generate_password_hash(password, method='sha256')))
-            sys.stderr.write(str(user.password))
             flash('Invalid Ussername or Password. Try Again.')
             return redirect(url_for('auth.login'))
 
@@ -49,34 +36,6 @@ def login():
 
 @auth.route("/signup", methods=['POST','GET'])
 def signup():
-    """
-    import psycopg2
-    db_config = os.environ['DATABASE_URL']
-    print(db_config)
-    conn = psycopg2.connect(db_config, sslmode='require')
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS test (food TEXT, rating INTEGER);")
-    cursor.execute("INSERT INTO test VALUES ('borgir', 9);")
-    cursor.execute("INSERT INTO test VALUES ('salad', 0);")
-    conn.commit()
-    cursor.execute("SELECT * FROM test;")
-    rows = str(cursor.fetchall())
-    sys.stderr.write(rows)
-    conn.close()
-    
-    if request.method == 'POST':
-        email = request.form['email']
-        username = request.form['username']
-        password1 = request.form['password1']
-        password2 = request.form['password2']
-        print(email, username, password1, password2)
-        if password1 == password2:
-            for user in fake_database:
-                if user['username'] == username:
-                    return render_template("Signup.html")
-            fake_database.append({'email': email,'username':username,'password':password1})
-            return redirect('/')
-    """
     if request.method == 'POST':
         email = request.form['email']
         username = request.form['username']
@@ -112,9 +71,12 @@ def signup():
         else:
             flash("Passwords do not match.")
             return redirect(url_for('auth.signup'))
-        
-
-        
-
 
     return render_template("Signup.html")
+
+@auth.route("/logout", methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
