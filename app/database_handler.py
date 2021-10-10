0102886d74aea0df9ea849fb8af4b3748bea69da
ethanhal <ethanhal@buffalo.edu>
2021-10-10 14:56:34 -0400

    #16 more changes to User initialization

--- app/database_handler.py
+++ app/database_handler.py
@@ -19,12 +19,13 @@ class User:
                 self.hashedPassword = user[3]
                 self.id = user[0]
 
-        elif type(id) is int and username == None and password == None:
-            user = get_user_by_id(id)
-            self.email = user[1]
-            self.username = user[2]
-            self.hashedPassword = user[3]
-            self.id =user[0]
+        elif username == None and password == None:
+            try:
+                user = get_user_by_id(int(id))
+                self.email = user[1]
+                self.username = user[2]
+                self.hashedPassword = user[3]
+                self.id =user[0]
         else:
             return None
     
