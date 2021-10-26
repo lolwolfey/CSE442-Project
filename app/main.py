3c7f85b4acf15dbad0e24688f238c54fcc659abd
ethanhal <ethanhal@buffalo.edu>
2021-10-26 10:55:56 -0400

    #35 added test plot to stats page

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from flask import Flask
 from flask import *
-#from . import db
 import os
 import sys
 from flask_login import login_user, login_required, logout_user, current_user
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
 
 main = Blueprint('main',__name__)
 
@@ -26,6 +28,22 @@ def stats():
 @login_required
 def settings():
     return render_template('Settings.html')
+
+@app.route('/plot.png')
+@login_required
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
 
 
 
