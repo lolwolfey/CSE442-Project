6c13ff512804b8cc5d3a54c42068ff9ef7b736bc
lolwolfey <rpradeep@buffalo.edu>
2021-10-27 16:18:32 -0400

    #36 fixed alignment of popular channels and fixed redirecting

--- app/main.py
+++ app/main.py
@@ -17,31 +17,41 @@ def home():
 def search():
     api_key = 'AIzaSyCrIwhrMNtHT0TX7HOJKhuMhWpKHvNjkXM'
     if request.method == "POST":
-        #print(request.form)
+        print(f"Request Form: {request.form}")
+        ytchannel = request.form.get("userName")
+        print(ytchannel)
+        #rint(f"REQ:{request.form['col']}")
+        if (request.form.get("col")):
+            ytchannel = request.form.get("col")
         #print(request.form.get("username"))
-        ytchannel = request.form.get("userName") #get the username field of form in search page
+        #ytchannel = request.form.get("userName") #get the username field of form in search page
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername={ytchannel}&key={api_key}"
         json_url = requests.get(url) #get the json data from url
         data = json.loads(json_url.text)
         #print(data)
-        channelID = data['items'][0]["id"] #channelID to use for plotting
+        channelID = data['items'][0]["id"] #channelID to use in linking to the YT channel
+        #print(channelID)
         subCount = data['items'][0]["statistics"]["subscriberCount"]
         viewCount = data['items'][0]["statistics"]["viewCount"]
         videoCount = data['items'][0]["statistics"]["videoCount"]
+    #channelPic = data['items'][0]["high"]["url"]
         channelPic = data['items'][0]["snippet"]["thumbnails"]["medium"]["url"]
-        infoTuple = (ytchannel,subCount,viewCount,videoCount,channelPic) #adds all the info into tuple and adds tuple to array
-        channels[0] = infoTuple
-        return redirect(url_for('stats'))
-        #print(channels)
 
-    #print(request.form.get()) 
-    return render_template('Search.html')
+        infoTuple = (ytchannel,subCount,viewCount,videoCount,channelPic,channelID)
+        channels[0] = infoTuple
+        print(channels)
+        return redirect(url_for('stats'))
+    #newurl = f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&relevanceLanguage=en&maxResults=5&key={api_key}"
+    #json_url2 = requests.get(newurl)
+    #data2 = json.loads(json_url2.text)
+    #print(data2)
+    return render_template("Search.html")
 
 @main.route('/stats')
 @login_required
 def stats():
     global channels
-    return render_template("Stats.html",Other_User=channels[0][0],subCounter=channels[0][1],viewCounter=channels[0][2],videoCounter=channels[0][3],thumbNail=channels[0][4])
+    return render_template("Stats.html",Other_User=channels[0][0],subCounter=channels[0][1],viewCounter=channels[0][2],videoCounter=channels[0][3],thumbNail=channels[0][4],channelID=channels[0][5])
 
 @main.route('/settings')
 @login_required
