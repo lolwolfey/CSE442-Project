ec2debe94957b02eb084622bee400111eba93ca3
lolwolfey <rpradeep@buffalo.edu>
2021-10-27 07:09:45 -0400

    #36 added popular channels frontend and channelID saved for plot

--- app/main.py
+++ app/main.py
@@ -19,16 +19,17 @@ def search():
     if request.method == "POST":
         #print(request.form)
         #print(request.form.get("username"))
-        ytchannel = request.form.get("username")
+        ytchannel = request.form.get("username") #get the username field of form in search page
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername={ytchannel}&key={api_key}"
-        json_url = requests.get(url)
+        json_url = requests.get(url) #get the json data from url
         data = json.loads(json_url.text)
         #print(data)
+        channelID = data['items'][0]["id"] #channelID to use for plotting
         subCount = data['items'][0]["statistics"]["subscriberCount"]
         viewCount = data['items'][0]["statistics"]["viewCount"]
         videoCount = data['items'][0]["statistics"]["videoCount"]
         channelPic = data['items'][0]["snippet"]["thumbnails"]["medium"]["url"]
-        infoTuple = (ytchannel,subCount,viewCount,videoCount,channelPic)
+        infoTuple = (ytchannel,subCount,viewCount,videoCount,channelPic) #adds all the info into tuple and adds tuple to array
         channels[0] = infoTuple
         return redirect(url_for('stats'))
         #print(channels)
