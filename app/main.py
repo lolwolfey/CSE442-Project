a244acb1c9d7d725faaf371dd08c9819c83b6eb7
liweijia12 <liweijia@buffalo.edu>
2021-11-02 14:11:41 -0400

    #39 - Adding stats function from other branches

--- app/main.py
+++ app/main.py
@@ -28,5 +28,40 @@ def settings():
     return render_template('Settings.html')
 
 
+@main.route('/plot.png')
+@login_required
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.set_title("Random Noise")
+    axis.set_xlabel("1 - 100")
+    axis.set_ylabel("Random #'s (1-50)")
+    axis.plot(xs, ys, )
+    return fig
 
+@main.route('/barplot.png')
+@login_required
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.set_title("Random Noise")
+    axis.set_xlabel("1 - 100")
+    axis.set_ylabel("Random #'s (1-50)")
+    axis.plot(xs, ys, )
+    return fig
