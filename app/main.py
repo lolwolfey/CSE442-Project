db54a93d72c4415e8f35ec4b71c599f1d4f52b2d
liweijia12 <liweijia@buffalo.edu>
2021-11-09 14:58:57 -0500

    #42 - moved reset into main settings route

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from flask import *
 import os
 import sys
 from flask_login import login_user, login_required, logout_user, current_user
+from .database_handler import bookmark_channel, init, signup_user, user_login, User, change_pass
+from werkzeug.security import generate_password_hash, check_password_hash
+import psycopg2
 
 main = Blueprint('main',__name__)
 
@@ -25,6 +28,21 @@ def stats():
 @main.route('/settings', methods = ['POST', 'GET'])
 @login_required
 def settings():
+     if request.method == 'POST':
+        username = request.form['usrname']
+        OldPass = request.form['oldpw']
+        NewPass = request.form['newpw']
+        user = User(None, username, None)
+        password = user.hashedPassword
+        flash('VALID password, everything up to now works!')
+#         # if check_password_hash(password, OldPass):
+#         #     valid, error = password_requirements(NewPass)
+#         #     if valid:
+#                 # change_pass(user.username,NewPass)
+#         #     else:
+#                 # flash('Invalid NEW Password!', 'error')
+#         # else:
+#         #     flash('Old password is not correct', 'error')
     return render_template('Settings.html')
 
 # @main.route("/SettingPassChange", methods = ['POST'])
