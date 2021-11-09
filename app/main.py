1ee8da9dcbc2d9bcd461c7255c94ed8e630a4e06
liweijia12 <liweijia@buffalo.edu>
2021-11-09 16:29:51 -0500

    #42 -  change form types and print values

--- app/main.py
+++ app/main.py
@@ -33,9 +33,9 @@ def settings():
         # username = request.form['usrname']
         OldPass = request.form['oldpw']
         NewPass = request.form['newpw']
-        user = User(None, current_user.username, None)
-        password = user.hashedPassword
-        flash('VALID password, everything up to now works!')
+        # user = User(None, current_user.username, None)
+        # password = user.hashedPassword
+        flash('VALID password, everything up to now works!'+ str(OldPass) + str(NewPass) + str(current_user.username))
         # if check_password_hash(password, OldPass):
         #      valid, error = password_requirements(NewPass)
         #      if valid:
